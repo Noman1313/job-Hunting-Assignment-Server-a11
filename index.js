f82bac62633520fe60bb763282f91259b7ed0b0f
Abdullah13130 <96126010+Abdullah13130@users.noreply.github.com>
2023-11-10 22:26:11 +0600

    add ajob and vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -10,14 +11,15 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        // 'http://localhost:5173'
+        'https://job-hunter-assignment-11.web.app',
+        'https://job-hunter-assignment-11.firebaseapp.com'
     ],
     credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
-// jobHunter
-// dvet15B8LGU3d3MO
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnwhdjk.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,10 +33,31 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+const logger = (req, res, next) => {
+    console.log('log info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token
+    // console.log('token in the middleware', token);
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized user' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized user' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const jobsCollection = client.db('jobsHunter').collection('jobs');
         const applyJobsCollection = client.db('jobsHunter').collection('applyJobs');
@@ -44,18 +67,21 @@ async function run() {
             const user = req.body;
             console.log('user for tokennnn', user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.cookie('token', token,{
+            res.cookie('token', token, {
                 httpOnly: true,
-                secure: true,
-                sameSite: 'none'
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             })
-            .send({ success: true })
+                .send({ success: true })
         })
 
-        app.post('/logout', async(req,res)=>{
+        app.post('/logout', async (req, res) => {
             const user = req.body;
             console.log('logout user', user);
-            res.clearCookie('token', {maxAge: 0}).send({success: true})
+            res.clearCookie('token', {
+                maxAge: 0, secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            }).send({ success: true })
         })
 
         //jobs api
@@ -80,8 +106,12 @@ async function run() {
         })
 
         //apply jobs
-        app.get('/applyJobs', async (req, res) => {
-            console.log(req.query.email);
+        app.get('/applyJobs', logger, verifyToken, async (req, res) => {
+            console.log(req.query?.email);
+            console.log('coocooocoo cookis owner', req.user);
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'Forbidden user' })
+            }
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -105,7 +135,7 @@ async function run() {
             res.send(result)
         })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
